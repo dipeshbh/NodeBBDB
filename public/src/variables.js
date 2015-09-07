5dd15e0a01d9eb47656d86c8dd90158953fbb1fb
dipeshbh <i836816@MacBook-Pro.local>
2015-09-07 16:38:07 -0700

    new repo

---
+++ public/src/variables.js
@@ -0,0 +1,14 @@
+"use strict";
+/*global ajaxify*/
+
+(function(ajaxify) {
+
+	ajaxify.variables = {};
+
+	ajaxify.variables.parse = function() {
+		var dataEl = $('#content [ajaxify-data]');
+		if (dataEl.length) {
+			ajaxify.data = JSON.parse(decodeURIComponent(dataEl.attr('ajaxify-data')));
+		}
+	};
+}(ajaxify || {}));
