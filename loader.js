89843f575f323c1cdc248a6e214f87b34533a559
dipeshbh <i836816@MacBook-Pro.local>
2015-09-23 23:41:17 -0700

    added new relic

--- loader.js
+++ loader.js
@@ -1,4 +1,5 @@
 'use strict';
+require('newrelic')
 
 var	nconf = require('nconf'),
 	fs = require('fs'),
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['nodebbdb'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '0ca94ad4e73f8b8f413cb7d25b2c7cb4b474e171',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
